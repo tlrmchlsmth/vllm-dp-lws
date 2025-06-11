ed044d1969a7e54fc2755e0f81a39021b358714d
Tyler Michael Smith <tysmith@redhat.com>
2025-06-11 19:55:12 +0000

    add dotfiles script
    Signed-off-by: Tyler Michael Smith <tysmith@redhat.com>

--- install-scripts/dotfiles.sh
+++ install-scripts/dotfiles.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 ##############################################################################
-# vLLM bootstrap script
-# - Installs vLLM
+# TMS's dotfiles script
 # - Idempotent: re-runs will update existing repos instead of recloning
 ##############################################################################
 
@@ -9,31 +8,21 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd  )"
 source ${SCRIPT_DIR}/common.sh
 
 ##############################  configuration  ###############################
-# Locations (override via env if desired)
-VLLM_SOURCE_DIR="${VLLM_SOURCE_DIR:-/app/vllm}"
 
-# Repositories
-VLLM_REPO_URL="${VLLM_REPO_URL:-https://github.com/vllm-project/vllm.git}"
-VLLM_BRANCH="${VLLM_BRANCH:-main}"
+DOTFILES_REPO_URL="${DOTFILES_REPO_URL:-https://github.com/tlrmchlsmth/dotfiles.git}"
+DOTFILES_SOURCE_DIR="${HOME}/dotfiles"
 
 banner "Environment summary"
 echo "Python version      : ${PYTHON_VERSION}"
 echo "Virtualenv path     : ${VENV_PATH}"
 echo "uv binary           : ${UV}"
-echo "vLLM repo / branch  : ${VLLM_REPO_URL}  (${VLLM_BRANCH})"
+echo "dotfiles repo       : ${DOTFILES_REPO_URL}"
 echo "====================================================================="
 
 ################################  vLLM  ######################################
-clone_or_update "${VLLM_REPO_URL}" "${VLLM_SOURCE_DIR}" "${VLLM_BRANCH}"
+clone_or_update "${DOTFILES_REPO_URL}" "${DOTFILES_SOURCE_DIR}"
 
-banner "Installing vLLM (editable)"
-pushd "${VLLM_SOURCE_DIR}" >/dev/null
-
-# TODO(tms): Work around for compressed_tensors bug in vLLM.
-# Remove when no longer needed
-upip accelerate
-
-upip -e .
+banner "Installing dotfiles"
+pushd "${DOTFILES_SOURCE_DIR}" >/dev/null
+bash install.sh
 popd >/dev/null
-
-banner "vLLM is ready"
